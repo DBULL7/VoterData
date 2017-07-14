67cacec376bdf4cf4fac5d3c08978d891921da16
Devon Bull <dbull@live.com>
2017-07-13 22:51:53 -0600

    district endpoints secured

--- server/controllers/controllerHelpers.js
+++ server/controllers/controllerHelpers.js
@@ -1,4 +1,6 @@
-const checkRequest = (req, res, next) => {
+let jwt = require('jsonwebtoken');
+
+const checkreq = (req, res, next) => {
   let distNum = req.params.num
   let checkType = Number(distNum)
   if (isNaN(checkType)) return res.status(400).json({message: 'pick a number from 1 to 7'})
@@ -6,6 +8,33 @@ const checkRequest = (req, res, next) => {
   next()
 }
 
+const checkAuth = (req, res, next) => {
+  const token = req.body.token ||
+                req.params.token ||
+                req.headers['authorization'];
+
+  if (token) {
+    jwt.verify(token, req.app.get('secretKey'), (error, decoded) => {
+      if (error) {
+        return res.status(403).send({
+          success: false,
+          message: 'Invalid authorization token.'
+        });
+      }
+      else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'You must be authorized to hit this endpoint'
+    });
+  }
+};
+
 module.exports = {
-  checkRequest: checkRequest
+  checkreq: checkreq,
+  checkAuth: checkAuth
 };
