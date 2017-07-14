7c2c26fab3e4a558b9970c7eeab29539fd78fe1a
Devon Bull <dbull@live.com>
2017-07-13 21:26:23 -0600

    district routes functional

--- server/router.js
+++ server/router.js
@@ -4,17 +4,21 @@ const vc = require('./controllers/voterController')
 const dc = require('./controllers/districtController')
 const helper = require('./controllers/controllerHelpers')
 
+r.get('/district', dc.getAllDistricts) // works
+r.get('/district/:num/:gender/', dc.getGender) // works need to make it able to get all voters using a range
+r.get('/district/:num/voters/', dc.getDistrictVoters) //works
+r.patch('/district/:id', dc.updateDistrict) // works
+r.post('/district', dc.newDistrict) // works but needs improvements
+r.get('/district/:num/party/party?:party', dc.getDistrictByParty) //works
+r.delete('/district/:id', dc.deleteDistrict) //works but verify again
+r.get('/district/:num', helper.checkRequest, dc.getDistrict) //works
 
 
-// patch a district
-// find gender in district
-// 
-r.post('/district/party?:party', dc.getDistrictByParty) //need to change this from post
-r.get('/district/:num/', helper.checkRequest, dc.getDistrict)
-
+r.get('/voters/range/:num')
 r.get('/voters', vc.getVoters)
 r.get('/voters/:voter', vc.getVoter)
 r.post('/voter', vc.newVoter)
+r.patch('/voter/:id')
 r.delete('/voters/:voter', vc.deleteVoter)
 
 module.exports = r;
